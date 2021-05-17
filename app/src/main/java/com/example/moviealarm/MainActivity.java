48eab567ee72c3170e8ba4258ff85d66f88c352e
null-miraiLABO <mizuki2002b@gmail.com>
2021-05-17 10:26:39 +0900

    9th commit

--- app/src/main/java/com/example/moviealarm/MainActivity.java
+++ app/src/main/java/com/example/moviealarm/MainActivity.java
@@ -37,9 +37,11 @@ public class MainActivity extends YouTubeBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button MakeTimerBt = findViewById(R.id.MakeTimerBt);
+        MakeTimeLintener lintener = new MakeTimeLintener();
+        MakeTimerBt.setOnClickListener(lintener);
+
         mTextViewCountDown=findViewById(R.id.text_view_countdown);
-        //mButtonStartPause=findViewById(R.id.button_start);
-        //getmButtonReset=findViewById(R.id.button_reset);
         mYouTubePlayerView=findViewById(R.id.youtube_view);
 
         //youtube
@@ -55,31 +57,7 @@ public class MainActivity extends YouTubeBaseActivity {
             }
         };
 
-        /*
-        //スタートボタンの中身
-        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println(mTimerRunning);
-                 if (mTimerRunning) {
-                     pauseTimer();
-                 }else {
-                     startTime();
-                 }
-            }
-        });
-        //リセットボタンの中身
-        getmButtonReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetTime();
-            }
-        });
-
-         */
-
         mYouTubePlayerView.initialize(API_KEY,mOnInitializedListener);
-        //updateCountDownText();
     }
 
     public void clickTimer(View view) {
@@ -91,68 +69,11 @@ public class MainActivity extends YouTubeBaseActivity {
         startActivity(intent);
     }
 
-    /*
-    //buttonをクリックした時
-    public void setClick(View view) {
-        if (mTimerRunning) {
-            pauseTimer();
-        }else{
-            startTime();
+    private class MakeTimeLintener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            MakeTimeDialogFragment dialogFragment = new MakeTimeDialogFragment();
+            dialogFragment.show(getSupportFragmentManager(),"MakeTimeDialogFragment");
         }
     }
-
-    //buttonをクリックした時
-    public void resetClick(View view) {
-        resetTime();
-    }
-
-    //タイマーの開始
-    private void startTime(){
-        mCountDownTimer=new CountDownTimer(mTimerLeftInMillis,1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                mTimerLeftInMillis=millisUntilFinished;
-                updateCountDownText();
-            }
-
-            @Override
-            public void onFinish() {
-                mTimerRunning=false;
-                mButtonStartPause.setText("スタート");
-                getmButtonReset.setVisibility(View.INVISIBLE);
-            }
-        }.start();
-
-        mTimerRunning=true;
-        mButtonStartPause.setText("一時停止");
-        getmButtonReset.setVisibility(View.INVISIBLE);
-    }
-
-    //一時停止
-    private void pauseTimer(){
-        mCountDownTimer.cancel();
-        mTimerRunning=false;
-        mButtonStartPause.setText("一時停止");
-        getmButtonReset.setVisibility(View.INVISIBLE);
-
-    }
-
-    //リセット
-    private void resetTime(){
-        mTimerLeftInMillis=START_TIME;
-        updateCountDownText();
-        mButtonStartPause.setVisibility(View.VISIBLE);
-        getmButtonReset.setVisibility(View.INVISIBLE);
-
-    }
-
-    //時刻の表示
-    private void updateCountDownText(){
-        int minutes = (int)(mTimerLeftInMillis/1000)/60;
-        int second  = (int)(mTimerLeftInMillis/1000)%60;
-        String timerLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,second);
-        mTextViewCountDown.setText(timerLeftFormatted);
-    }
-
-     */
 }
